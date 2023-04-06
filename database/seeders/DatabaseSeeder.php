95291f3ae6770551bd0b44ff449243360fc71589
Robbi Chandra <robbiechandra02@gmail.com>
2023-04-07 03:23:46 +0700

    first commit

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,13 +14,13 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::create([
-        //     'username' => 'User',
-        //     'email' => 'user@gmail.com',
-        //     'password' => Hash::make('user'),
-        //     'email_verified_at' => now(),
-        //     'remember_token' => Str::random(10),
-        // ]);
+        \App\Models\User::create([
+            'username' => 'User',
+            'email' => 'user@gmail.com',
+            'password' => Hash::make('user'),
+            'email_verified_at' => now(),
+            'remember_token' => Str::random(10),
+        ]);
 
         \App\Models\RumahSakit::factory(20)->create();
         \App\Models\Pasien::factory(20)->create();
